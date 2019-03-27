6dc53addf72169e1efe71317a22c5fb964ceb6a8
IsaacDdR <isaacovic00@gmail.com>
2019-03-27 13:29:11 -0600

    Vulkan test :

--- main.cpp
+++ main.cpp
@@ -1,96 +1,92 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <iostream> 
+#include <iostream>
 #include <stdexcept>
 #include <cstdlib>
-
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
-	void run(){
-		initWindow();
-		initVulkan();
-		mainLoop();
-		cleanup();
-	}
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
 
 private:
-	VkInstance instance;
+    GLFWwindow* window;
 
-	GLFWwindow* window;
-		
-	void initWindow(){
-		glfwInit();
+    VkInstance instance;
 
-		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    void initWindow() {
+        glfwInit();
 
-		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
-	}
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-	void initVulkan(){
-		createInstance();
-	}
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
 
-	
-	void mainLoop(){
-		while (!glfwWindowShouldClose(window)){
-			glfwPollEvents();
-		}
-	}
+    void initVulkan() {
+        createInstance();
+    }
 
-	void cleanup(){
-		glfwDestroyWindow(window);
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+    }
 
-		glfwTerminate();
-	}
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
 
-	void createInstance(){
-		VkApplicationInfo appInfo = {};
-		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-		appInfo.pApplicationName = "Hello Triangle";
-		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.pEngineName = "No engine";
-		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-		appInfo.apiVersion = VK_API_VERSION_1_0;
+        glfwDestroyWindow(window);
 
-		VkInstanceCreateInfo createInfo = {};
-		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-		createInfo.pApplicationInfo = &appInfo;
+        glfwTerminate();
+    }
 
-		uint32_t glfwExtensionCount = 0;
-		const char** glfwExtensions;
+    void createInstance() {
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
 
-		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
 
-		createInfo.enabledExtensionCount = glfwExtensionCount;
-		createInfo.ppEnabledExtensionNames = glfwExtensions;
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-		createInfo.enabledLayerCount = 0;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
 
-		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
-			throw std::runtime_error("Failed to create instance!");
-		}
+        createInfo.enabledLayerCount = 0;
 
-	 }
-
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
 };
 
-int main(){
-	HelloTriangleApplication app;
+int main() {
+    HelloTriangleApplication app;
 
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	try {
-		app.run();
-	} catch (const std::exception& e){
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS; 
+    return EXIT_SUCCESS;
 }
 
